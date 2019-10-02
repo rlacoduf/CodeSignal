3110afcfe86a92fae3a49aea35630e99d0f9964f
yeolchae <kimchaeyeol92@gmail.com>
2019-10-03 01:09:03 +0900

    19.10.03 이전 작업물

--- CodeSignal/src/com/codesignal/app/arcade/Intro/CommonCharacterCount.java
+++ CodeSignal/src/com/codesignal/app/arcade/Intro/CommonCharacterCount.java
@@ -1,5 +1,54 @@
 package com.codesignal.app.arcade.Intro;
 
-public class CommonCharacterCount {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class CommonCharacterCount {
+	int commonCharacterCount(String s1, String s2) {
+		int result = 0;
+		List<Character> str1 = new ArrayList<Character>();
+		List<Character> str2 = new ArrayList<Character>();
+		char[] st1 = s1.toCharArray();
+		char[] st2 = s2.toCharArray();
+		Arrays.parallelSort(st1);
+		Arrays.parallelSort(st2);
+		for (int i = 0; i < st1.length; i++) {
+			str1.add(st1[i]);
+		}
+		for (int i = 0; i < st2.length; i++) {
+			str2.add(st2[i]);
+		}
+		for (int i = 0; i < str1.size(); i++) {
+			for (int j = 0; j < str2.size(); j++) {
+				if (str1.get(i)==str2.get(j)) {
+					result++;
+					str2.remove(j);
+					if (str2.size()==0) {
+						return result;
+					}
+					break;
+				}
+			}
+		}
+		return result;
+	}
+	
+	//this is Copy of user 'viet_tran'
+	int commonCharacterCount1(String s1, String s2) {
+	    int[] a = new int[26], 
+	        b = new int[26];
+	    for (char c: s1.toCharArray())
+	        a[c - 'a']++; // for count char num
+	    for (char c: s2.toCharArray())
+	        b[c - 'a']++;
+	    int s = 0;
+	    for (int i = 0; i < 26; ++i) 
+	        s +=Math.min(a[i],b[i]); 
+	    //ex i = 1 -> a[1],b[1]-> 'b' letter num
+	    //if String 's1' and 's2' has letters 'b', a[1]=2, b[1]=0
+	    // -> result s = 0 cuz letter 'b' is not concurrectly exist LOL
+	    return s;
+	        
+	}
 }
--- CodeSignal/src/com/codesignal/app/arcade/Intro/IsLucky.java
+++ CodeSignal/src/com/codesignal/app/arcade/Intro/IsLucky.java
@@ -1,5 +1,36 @@
 package com.codesignal.app.arcade.Intro;
 
 public class IsLucky {
+	static boolean isLucky(int n) {
+		int tmp = n;
+		int cnt = 0;
+		while (tmp != 0) {
+			cnt++;
+			tmp /= 10;
+		}
+		int num1 = 0, num2 = 0;
+		num1 = (int) (n / Math.pow(10, cnt / 2));
+		num2 = (int) (n % Math.pow(10, cnt / 2));
 
+		int sum1 = 0, sum2 = 0;
+		while (num1 != 0) {
+			sum1 += num1 % 10;
+			sum2 += num2 % 10;
+
+			num1 = num1 / 10;
+			num2 = num2 / 10;
+		}
+		return sum1 == sum2;
+	}
+
+	// this is user jocke93's answer
+	boolean isLucky1(int n) {
+		String s = n + "";
+		int sum = 0;
+
+		for (int i = 0; i < s.length() / 2; i++)
+			sum += (s.charAt(i) - s.charAt(s.length() - 1 - i));
+		
+		return sum == 0;
+	}
 }
